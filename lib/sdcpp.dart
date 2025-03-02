c5238fb8866292d6fe85134aedbb2c2ae004d5b5
danemadsen <dane_madsen@hotmail.com>
2025-03-03 09:56:10 +1000

    image extension

--- lib/sdcpp.dart
+++ lib/sdcpp.dart
@@ -2,6 +2,8 @@ library;
 
 import 'dart:ffi' as ffi;
 import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:ffi/ffi.dart';
 
@@ -11,5 +13,6 @@ part 'src/stable_diffusion_exception.dart';
 part 'src/types/schedule_type.dart';
 part 'src/stable_diffusion_native.dart';
 part 'src/context_params.dart';
+part 'src/image_extension.dart';
 part 'src/types/stable_diffusion_type.dart';
 part 'src/stable_diffusion.dart';
