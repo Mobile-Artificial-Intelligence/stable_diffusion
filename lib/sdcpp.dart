ba53a01802e3f01836719f767336a37ee9750508
danemadsen <dane_madsen@hotmail.com>
2025-03-03 10:55:27 +1000

    mv

--- lib/sdcpp.dart
+++ lib/sdcpp.dart
@@ -11,6 +11,6 @@ import 'src/bindings.dart';
 part 'src/stable_diffusion_exception.dart';
 part 'src/types/schedule_type.dart';
 part 'src/stable_diffusion_native.dart';
-part 'src/context_params.dart';
+part 'src/params/context_params.dart';
 part 'src/types/stable_diffusion_type.dart';
 part 'src/stable_diffusion.dart';
