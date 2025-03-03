1c38ca2c6fad2c3dbcdf7b517e9ae17202a98923
danemadsen <dane_madsen@hotmail.com>
2025-03-03 14:41:02 +1000

    diffusion params

--- lib/sdcpp.dart
+++ lib/sdcpp.dart
@@ -2,15 +2,19 @@ library;
 
 import 'dart:ffi' as ffi;
 import 'dart:io';
+import 'dart:math' as math;
 import 'dart:typed_data';
 
 import 'package:ffi/ffi.dart';
+import 'package:flutter/foundation.dart';
 
 import 'src/bindings.dart';
 
 part 'src/stable_diffusion_exception.dart';
 part 'src/types/schedule_type.dart';
+part 'src/types/sample_method.dart';
 part 'src/stable_diffusion_native.dart';
 part 'src/params/context_params.dart';
+part 'src/params/diffusion_params.dart';
 part 'src/types/stable_diffusion_type.dart';
 part 'src/stable_diffusion.dart';
