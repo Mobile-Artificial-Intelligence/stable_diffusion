cb5be112ec9213dd5fc6ce13cf4dc860ca71d093
danemadsen <dane_madsen@hotmail.com>
2025-03-03 10:19:43 +1000

    rm

--- lib/sdcpp.dart
+++ lib/sdcpp.dart
@@ -3,7 +3,6 @@ library;
 import 'dart:ffi' as ffi;
 import 'dart:io';
 import 'dart:typed_data';
-import 'dart:ui';
 
 import 'package:ffi/ffi.dart';
 
@@ -13,6 +12,5 @@ part 'src/stable_diffusion_exception.dart';
 part 'src/types/schedule_type.dart';
 part 'src/stable_diffusion_native.dart';
 part 'src/context_params.dart';
-part 'src/image_extension.dart';
 part 'src/types/stable_diffusion_type.dart';
 part 'src/stable_diffusion.dart';
