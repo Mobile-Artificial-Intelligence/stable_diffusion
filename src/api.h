3996dcfb3edbf1a19d906cc7d45e4e4753549d10
danemadsen <dane_madsen@hotmail.com>
2025-03-18 11:47:18 +1000

    start on c_api

---
+++ src/api.h
@@ -0,0 +1,24 @@
+#ifndef SD_API_H
+#define SD_API_H
+
+#if defined(_WIN32) && !defined(__MINGW32__)
+    #define SD_DART_API __declspec(dllimport)
+#else
+    #define SD_DART_API __attribute__ ((visibility ("default")))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+SD_DART_API int stable_diffusion_init(char * params);
+
+SD_DART_API int stable_diffusion_txt2img(char * prompt, char * negative_prompt, char * params);
+
+SD_DART_API void stable_diffusion_free();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
