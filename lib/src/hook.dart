5a15ebdd9a23421957bace882c2217cc5949aad1
danemadsen <dane_madsen@hotmail.com>
2025-03-18 18:19:18 +1000

    more

--- lib/src/hook.dart
+++ lib/src/hook.dart
@@ -9,11 +9,9 @@ stable_diffusion? _lib;
 stable_diffusion get lib {
   if (_lib == null) {
     if (TargetPlatform.windows == defaultTargetPlatform) {
-      // _lib = stable_diffusion(ffi.DynamicLibrary.open('llama.dll'));
-      throw Exception('Not Yet Supported');
+      _lib = stable_diffusion(ffi.DynamicLibrary.open('stable-diffusion.dll'));
     } else if (TargetPlatform.linux == defaultTargetPlatform || TargetPlatform.android == defaultTargetPlatform) {
-      //_lib = stable_diffusion(ffi.DynamicLibrary.open('libllama.so'));
-      throw Exception('Not Yet Supported');
+      _lib = stable_diffusion(ffi.DynamicLibrary.open('libstable-diffusion.so'));
     } else if (TargetPlatform.iOS == defaultTargetPlatform || TargetPlatform.macOS == defaultTargetPlatform) {
       _lib = stable_diffusion(ffi.DynamicLibrary.open('sdcpp.framework/sdcpp'));
     } else {
